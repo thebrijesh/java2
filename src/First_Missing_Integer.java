fa650e36fa9d6b036c6c05d9e8a2d462ba03658e
91971 <brijeshpatel0045@gmail.com>
2023-07-28 17:35:06 +0530

    add pseudocode

---
+++ src/First_Missing_Integer.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class First_Missing_Integer {
+public static void pseudocode(){
+    //i will sort the array because i finding first missing element in array.
+    // make a variable; smallestMissing =0;
+    //after take a for each loop and check (num == smallestMissing) is true so smallestMissing++;
+    // is not true so return this number
+}
+    public static int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+
+        int smallestMissing = 1;
+        for (int num : nums) { 
+            if (num == smallestMissing) {
+                smallestMissing++;
+            }
+        }
+
+        return smallestMissing;
+    }
+    public static void main(String[] args) {
+        int[] nums2 = {3, 4, -1, 1};
+        System.out.println(firstMissingPositive(nums2));
+    }
+}
