d7c4f8fa7607e047fc462c87ecdb51348830911e
91971 <brijeshpatel0045@gmail.com>
2023-08-15 07:21:54 +0530

    find single number using xor

---
+++ src/SingleNumber.java
@@ -0,0 +1,13 @@
+public class SingleNumber {
+    public static void singalNumber(int[] arr){
+        int xor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            xor ^= arr[i];
+        }
+        System.out.println(xor);
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,1,3,4,5,3,4};
+        singalNumber(arr);
+    }
+}
