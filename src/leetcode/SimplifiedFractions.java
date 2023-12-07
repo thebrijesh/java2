c97603c49609b137a0c7109cacded4a7e4d0cfac
brijesh <brijeshpatel045@gmail.com>
2023-12-07 11:50:21 +0530

    1447. Simplified Fractions https://leetcode.com/problems/simplified-fractions/description/?envType=list&envId=pp0x5fed

--- src/leetcode/SimplifiedFractions.java
+++ src/leetcode/SimplifiedFractions.java
@@ -1,4 +1,32 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimplifiedFractions {
+
+
+    public static List<String> simplifiedFractions(int n) {
+        List<String> l = new ArrayList<>();
+        for (int i = 1; i <n; i++) {
+            for (int j = i+1; j <= n; j++) {
+                if (gcd(i, j) == 1) {
+                    String s = "" + i + "/" + j;
+                    l.add(s);
+                }
+            }
+        }
+        return l;
+    }
+
+    public static int gcd(int i, int j) {
+        if (j == 0) return i;
+        return gcd(j, i % j);
+    }
+
+
+    public static void main(String[] args) {
+        int n = 4;
+        simplifiedFractions(n);
+    }
 }
