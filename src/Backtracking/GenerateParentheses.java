0f3316cadde7e23297eb29fda8446a75d7c042cd
brijesh <brijeshpatel045@gmail.com>
2023-12-21 14:42:24 +0530

    Given an integer A pairs of parentheses, write a function to generate all combinations of well-formed parentheses of length 2*A.

--- src/Backtracking/GenerateParentheses.java
+++ src/Backtracking/GenerateParentheses.java
@@ -1,5 +1,45 @@
 package Backtracking;
 
-public class GenerateParentheses {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GenerateParentheses {
+    static List<String> ans = new ArrayList<String>();
+    static char value = '"';
+
+    public static void rec(int n, int ob, int cb, StringBuilder s) {
+        System.out.println("n: " + n + " ||" + " ob: " + ob + " ||" + " cb: " + cb + " ||" + " str: " + value + s + value);
+        if (ob > n || cb > n) {
+            return;
+        }
+        if (n == ob && ob == cb) {
+            ans.add(s.toString());
+            System.out.println(ans);
+            return;
+        }
+        if (ob > cb) {
+            s.append(")");
+            rec(n, ob, cb + 1, s);
+
+            s.deleteCharAt(s.length() - 1);
+            s.append("(");
+            rec(n, ob + 1, cb, s);
+            s.deleteCharAt(s.length() - 1);
+        } else {
+            s.append("(");
+            rec(n, ob + 1, cb, s);
+            s.deleteCharAt(s.length() - 1);
+        }
+        return;
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        StringBuilder temp = new StringBuilder();
+        rec(n, 0, 0, temp);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        generateParenthesis(3);
+    }
 }
